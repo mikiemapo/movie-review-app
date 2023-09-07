3246e25106fd8e777523dee2a328e9d62ba7639b
mikiemapo <123407631+mikiemapo@users.noreply.github.com>
2023-09-06 20:49:26 -0400

    First hello world working

---
+++ src/test/java/dev/mikiemapo/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mikiemapo.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
